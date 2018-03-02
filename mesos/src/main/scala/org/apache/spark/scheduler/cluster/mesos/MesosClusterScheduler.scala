cb1363d95161986ef49a4f203ebf90185a3678c5
Joe Buszkiewic <jbuszkiewic@rms.com>
2018-03-02 08:22:38 -0800

    use 80% of the requested memory for the spark driver

--- mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
+++ mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
@@ -451,7 +451,7 @@ private[spark] class MesosClusterScheduler(
       "--name", desc.conf.get("spark.app.name"),
       "--master", s"mesos://${conf.get("spark.master")}",
       "--driver-cores", desc.cores.toString,
-      "--driver-memory", s"${desc.mem}M")
+      "--driver-memory", s"${desc.mem * .8}M")
 
     // Assume empty main class means we're running python
     if (!desc.command.mainClass.equals("")) {
