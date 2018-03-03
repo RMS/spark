8592aefcc2356218fc089d3ecc2ea45685f1c68e
cgeorge <chris.george@rms.com>
2018-03-02 22:13:50 -0700

    adding support for driver memory overhead fraction... defaults to 20% over for memory... controllable via spark.mesos.driver.memoryOverhead default value = 0.2

--- mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
+++ mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
@@ -451,8 +451,7 @@ private[spark] class MesosClusterScheduler(
     var options = Seq(
       "--name", desc.conf.get("spark.app.name"),
       "--master", s"mesos://${conf.get("spark.master")}",
-      "--driver-cores", desc.cores.toString,
-      "--driver-memory", s"${desc.mem * .8}M")
+      "--driver-cores", desc.cores.toString)
 
     logTrace(s"driver cmd options ${options.mkString(",")}")
 
